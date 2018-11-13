ce340f0224cff1ab65149e2d81eec09b21a0609c
= <=>
2018-11-13 08:50:25 -0500

    Changed section classes into ids, added fadein, imported animate.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,21 +1,24 @@
 
 var $ = jQuery;
 $(document).ready(function () {
+    /*Fades in the page when the document is ready*/
+    $('div.hidden').fadeIn(2000).removeClass('hidden');
+    /*Changes the background color of the navigation*/
     $(function () {
         $(document).scroll(function () {
             var $nav = $(".navbar");
             $nav.toggleClass('navbar-scrolled', $(this).scrollTop() > $nav.height());
         });
     });
-    $(function () {
-        $(body).fadeIn(300, null);
-    });
+    /*Makes smooth scrolling only when we click each anchor or button*/
+    var anchor;
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
-});
+    
 });
